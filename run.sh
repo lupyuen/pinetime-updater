05d44a4cca0cc53ba9d789403963e1de2ede7c59
Lee Lup Yuen <luppy@appkaki.com>
2020-07-29 14:29:27 +0800

    Updating scripts

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@
 set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
 
+dialog --menu "What would you like to flash to PineTime today?" 0 0 0 1 "Latest Bootloader" 2 "Latest Firmware (FreeRTOS)" 3 "Download from URL" 4 "Downloaded file"
+
 #  Configure SWD Programmer
 #  Select ST-Link v2 as SWD Programmer
 swd_device=scripts/swd-stlink.ocd
@@ -11,7 +13,11 @@ swd_device=scripts/swd-stlink.ocd
 #  Select Raspberry Pi as SWD Programmer
 #  swd_device=scripts/swd-pi.ocd
 
+#  TODO: Download xPack OpenOCD
+
+#  TODO: Build openocd-spi
+
 #  Flash the device
 xpack-openocd/bin/openocd \
     -f $swd_device \
-    -f scripts/flash.ocd
+    -f scripts/flash-app.ocd
