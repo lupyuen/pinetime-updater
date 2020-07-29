e390f3c32bc0c9de7ff00251eb325de00af88c18
lupyuen <luppy@appkaki.com>
2020-07-29 19:12:29 +0800

    Tested OK on macOS

--- run.sh
+++ run.sh
@@ -1,72 +1,89 @@
 #!/usr/bin/env bash
-#  Flash to PineTime ROM (nRF52) with xPack OpenOCD (macOS / Linux) and openocd-spi (Raspberry Pi)
+#  Flash Bootloader and Application Firmware to PineTime ROM (nRF52) with xPack OpenOCD (macOS / Linux with ST-Link) and openocd-spi (Raspberry Pi)
 
 set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
 
-#  TODO: Fetch the latest bootload and firmware versions
+#  TODO: Fetch the latest bootloader and application firmware versions
 #  git pull
 
 function main {
     #  Install neofetch
     install_neofetch
 
-    #  Check whether this a Raspberry Pi
-    model=`neofetch model`
-    
-    #  model: Raspberry Pi 4 Model B Rev 1.1 
-    #  model: Pine64 Pinebook Pro
-    #  model: MacBookPro10,1
+    #  Get the machine model to check whether this a Raspberry Pi
+    model="$(neofetch model)"; model="${model##*: }"
+    set +x; echo; echo "----- Model is $model"; set -x
+
+    #  Sets model to...
+    #  Raspberry Pi 4 Model B Rev 1.1 
+    #  Pine64 Pinebook Pro
+    #  MacBookPro10,1
 
     #  Configure SWD Programmer
-
-    #  If Raspberry Pi:
-    #  Select Raspberry Pi as SWD Programmer
-    #  swd_device=scripts/swd-pi.ocd
-    #  Download openocd-spi
-    #  install_openocd_spi
-
-    #  If not Raspberry Pi:
-    #  Select ST-Link v2 as SWD Programmer
-    swd_device=scripts/swd-stlink.ocd
-    #  Download xPack OpenOCD
-    install_openocd
+    local swd_device=
+    local openocd=
+    if [[ "$model" == Raspberry\ Pi* ]]; then
+        #  If Raspberry Pi: Select openocd-spi as SWD Programmer
+        openocd=openocd-spi/bin/openocd
+        swd_device=scripts/swd-pi.ocd
+        #  Download openocd-spi
+        install_openocd_spi
+    else
+        #  If not Raspberry Pi: Select xPack OpenOCD with ST-Link v2 as SWD Programmer
+        openocd=xpack-openocd/bin/openocd
+        swd_device=scripts/swd-stlink.ocd
+        #  Download xPack OpenOCD
+        install_openocd
+    fi
 
     #  Show menu
+    local input=/tmp/pinetime-updater.$$
     dialog \
         --menu "What would you like to flash to PineTime today?" \
         0 0 0 \
         1 "Latest Bootloader" \
         2 "Latest Firmware (FreeRTOS)" \
         3 "Download from URL" \
-        4 "Downloaded file"
+        4 "Downloaded file" \
+        2>$input
+    local selection=$(<"$input")
 
-    #  TODO: Download the file
-    #  For Bootloader:
-    #  wget https://github.com/lupyuen/pinetime-rust-mynewt/releases/download/v4.1.7/mynewt_nosemi.elf.bin
-    #  For Application Firmware:
-    #  wget https://github.com/JF002/Pinetime/releases/download/0.7.1/pinetime-mcuboot-app.img
+    #  Set the URL, filename and address
+    local url=
+    local filename=
+    local address=
+    case "$selection" in
+        1)  #  For Bootloader
+            url=https://github.com/lupyuen/pinetime-rust-mynewt/releases/download/v4.1.7/mynewt_nosemi.elf.bin
+            filename=/tmp/mynewt_nosemi.elf.bin
+            address=0x0
+            ;;
+        2)  #  For Application Firmware
+            url=https://github.com/JF002/Pinetime/releases/download/0.7.1/pinetime-mcuboot-app.img
+            filename=/tmp/pinetime-mcuboot-app.img
+            address=0x8000
+            ;;
+        3)  #  Download from URL
+            echo "TODO"; exit
+            address=0x8000
+            ;;
+        4)  #  Downloaded file
+            echo "TODO"; exit
+            address=0x8000
+            ;;
+        *)  #  Cancel
+            exit 0
+    esac
 
+    #  Download the URL
     #  TODO: If the URL is a GitHub Actions Artifact, prompt for GitHub Token
-
-    #  TODO: Set the flash filename and address
-    filename="a.img"
-    #  For Bootloader:
-    #  address=0x0
-    #  For Application Firmware:
-    address=0x8000
+    set +x; echo; echo "----- Downloading $url..."; set -x
+    wget $url -O $filename
 
     #  Flash the device    
-
-    #  If Raspberry Pi:
-    #  openocd_spi/bin/openocd \
-    #      -c " set filename \"$filename\" " \
-    #      -c " set address  \"$address\" " \
-    #      -f $swd_device \
-    #      -f scripts/flash-program.ocd
-
-    #  If not Rasperry Pi:
-    xpack-openocd/bin/openocd \
+    set +x; echo; echo "----- Flashing $filename..."; set -x
+    "$openocd" \
         -c " set filename \"$filename\" " \
         -c " set address  \"$address\" " \
         -f $swd_device \
@@ -113,10 +130,10 @@ function install_openocd {
     mv xpack-openocd-$version xpack-openocd
 }
 
-#  Download and build openocd_spi from github.com/lupyuen/openocd-spi
+#  Download and build openocd-spi from github.com/lupyuen/openocd-spi
 function install_openocd_spi {
     #  Return if already installed
-    if [ -e openocd_spi/bin/openocd ]; then
+    if [ -e openocd-spi/bin/openocd ]; then
         return
     fi
     set +x; echo; echo "----- Installing openocd-spi..."; set -x
