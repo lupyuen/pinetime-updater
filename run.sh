44208c891d365294a426b7bf63013b3291ac7451
lupyuen <luppy@appkaki.com>
2020-07-29 14:45:55 +0800

    Updating scripts

--- run.sh
+++ run.sh
@@ -4,20 +4,59 @@
 set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
 
-dialog --menu "What would you like to flash to PineTime today?" 0 0 0 1 "Latest Bootloader" 2 "Latest Firmware (FreeRTOS)" 3 "Download from URL" 4 "Downloaded file"
+#  TODO: git pull
 
-#  Configure SWD Programmer
-#  Select ST-Link v2 as SWD Programmer
-swd_device=scripts/swd-stlink.ocd
+function main {
+    #  Configure SWD Programmer
+    #  Select ST-Link v2 as SWD Programmer
+    local swd_device=scripts/swd-stlink.ocd
 
-#  Select Raspberry Pi as SWD Programmer
-#  swd_device=scripts/swd-pi.ocd
+    #  Select Raspberry Pi as SWD Programmer
+    #  local swd_device=scripts/swd-pi.ocd
 
-#  TODO: Download xPack OpenOCD
+    #  Download xPack OpenOCD
+    #  install_openocd
 
-#  TODO: Build openocd-spi
+    #  TODO: Build openocd-spi
 
-#  Flash the device
-xpack-openocd/bin/openocd \
-    -f $swd_device \
-    -f scripts/flash-app.ocd
+    dialog --menu "What would you like to flash to PineTime today?" 0 0 0 1 "Latest Bootloader" 2 "Latest Firmware (FreeRTOS)" 3 "Download from URL" 4 "Downloaded file"
+
+    #  Flash the device
+    xpack-openocd/bin/openocd \
+        -f $swd_device \
+        -f scripts/flash-app.ocd
+}
+
+function install_openocd {
+    #  Download xPack OpenOCD from https://xpack.github.io/openocd/install/
+    if [[ $(uname -m) == aarch64 ]];then
+        rm xpack-openocd-0.10.0-14-linux-arm64.tar.gz
+        wget https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.10.0-14/xpack-openocd-0.10.0-14-linux-arm64.tar.gz
+        tar -xf xpack-openocd-0.10.0-14-linux-arm64.tar.gz
+        rm xpack-openocd-0.10.0-14-linux-arm64.tar.gz
+        mv xpack-openocd-0.10.0-14 xpack-openocd
+    else
+        echo TODO
+    fi
+
+    #  macOS:
+    #  wget https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.10.0-14/xpack-openocd-0.10.0-14-darwin-x64.tar.gz
+
+    #  Linux Arm32:
+    #  wget https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.10.0-14/xpack-openocd-0.10.0-14-linux-arm.tar.gz
+
+    #  Linux Arm64:
+    #  wget https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.10.0-14/xpack-openocd-0.10.0-14-linux-arm64.tar.gz
+
+    #  Linux x32:
+    #  wget https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.10.0-14/xpack-openocd-0.10.0-14-linux-x32.tar.gz
+
+    #  Linux x64:
+    #  wget https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.10.0-14/xpack-openocd-0.10.0-14-linux-x64.tar.gz
+
+    #  Win32:
+    #  https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.10.0-14/xpack-openocd-0.10.0-14-win32-x32.zip
+
+    #  Win64:
+    #  https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.10.0-14/xpack-openocd-0.10.0-14-win32-x64.zip
+}
