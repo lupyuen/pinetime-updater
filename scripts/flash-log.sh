d3c6afca5ce48c220e11b7904974035de15fedbf
lupyuen <luppy@appkaki.com>
2020-10-01 12:59:41 +0800

    Test

--- scripts/flash-log.sh
+++ scripts/flash-log.sh
@@ -7,5 +7,6 @@ set -x  #  Echo all commands.
 xpack-openocd/bin/openocd
     -c ' set filename "firmware.bin" ' 
     -c ' set address  "0x0" ' 
+    -c ' add_script_search_dir xpack-openocd/scripts '
     -f scripts/swd-stlink.ocd 
     -f scripts/flash-program.ocd
