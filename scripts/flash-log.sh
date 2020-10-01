dc4629a175261a4f87f6c7cfdb0c5d0a401ce4e6
lupyuen <luppy@appkaki.com>
2020-10-01 13:04:27 +0800

    Test

--- scripts/flash-log.sh
+++ scripts/flash-log.sh
@@ -4,9 +4,8 @@
 set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
 
-xpack-openocd/bin/openocd
-    -c ' set filename "firmware.bin" ' 
-    -c ' set address  "0x0" ' 
-    -c ' add_script_search_dir xpack-openocd/scripts '
-    -f scripts/swd-stlink.ocd 
+xpack-openocd/bin/openocd \
+    -c ' set filename "firmware.bin" ' \
+    -c ' set address  "0x0" ' \
+    -f scripts/swd-stlink.ocd \
     -f scripts/flash-program.ocd
